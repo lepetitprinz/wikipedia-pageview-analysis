a8ce5cc3846350f4a89f5821402a6f8169676ef2
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-07 16:42:49 +0900

    Update kube_pod_operator_test.py

--- dags/kube_pod_operator_test.py
+++ dags/kube_pod_operator_test.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from kubernetes.client import models as k8s
 from airflow.models import DAG, Variable
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.kubernetes.secret import Secret
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
 dag_id = 'kubernetes-test-dag'
@@ -24,15 +23,8 @@ dag = DAG(
     dag_id=dag_id,
     description='kubernetes pod operator',
     default_args=task_default_args,
-    schedule_interval='5 16 * * *',
+    schedule_interval='0 0 * * *',
     max_active_runs=1
-)
-
-env = Secret(
-    'env',
-    'TEST',
-    'test_env',
-    'TEST',
 )
 
 resources = k8s.V1ResourceRequirements(
@@ -40,29 +32,29 @@ resources = k8s.V1ResourceRequirements(
     requests={"memory": "500Mi", "cpu": "0.5"},
 )
 
-env_from = [
-    k8s.V1EnvFromSource(
-        config_map_ref=k8s.V1ConfigMapEnvSource(name='secret')),
-]
-
 start = DummyOperator(task_id="start", dag=dag)
 
 run = KubernetesPodOperator(
+    # The ID specified for the task
     task_id="kubernetes-pod-operator",
-    name="example", 
+    image='debian',
+    image_pull_policy="IfNotPresent",
+    # launch the pod on the same cluster as Airflow is running
+    in_cluster=True,
     namespace='airflow',
+    # Pod configuration
+    # name of task you want to run, used to generate Pod ID
+    name="example", 
     cmds=["bash", "-cx"],
     arguments=["echo", "10"],
     service_account_name="airflow",
-    is_delete_operator_pod=True,
-    image='debian',
-    image_pull_policy="IfNotPresent",
-    secrets=[env],
-    get_logs=True,
     container_resources=resources,
-    env_from=env_from,
-    dag=dag,
+    # delete Pod after the task is finished
+    is_delete_operator_pod=True,
+    # determines whether to use the stdout of the container as task-logs to the Airflow logging system
+    get_logs=True,
     do_xcom_push=True,
+    dag=dag,
 )
 
 start >> run
