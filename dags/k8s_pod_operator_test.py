33e26fdcfcc9d06bbb7882a382de16fb4eb14846
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-07 13:58:00 +0900

    create airflow test dag about kubernetes pod operator

---
+++ dags/k8s_pod_operator_test.py
@@ -0,0 +1,13 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+k = KubernetesPodOperator(
+    name="hello-dry-run",
+    image="debian",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    task_id="dry_run_demo",
+    do_xcom_push=True,
+)
+
+k.dry_run()
