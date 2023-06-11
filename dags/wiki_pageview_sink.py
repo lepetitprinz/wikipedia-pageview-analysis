16db3a9c6881f48a05a82d19d53877838adc9302
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-12 03:03:50 +0900

    revise the env_vars argument and change the k8s pod image

--- dags/wiki_pageview_sink.py
+++ dags/wiki_pageview_sink.py
@@ -3,9 +3,7 @@ from datetime import timedelta
 import airflow.utils.dates
 from airflow import DAG
 from kubernetes.client import models as k8s
-from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from minio import Minio
 
 
 default_args = {
@@ -14,44 +12,23 @@ default_args = {
     "retries": 3,
     "retry_delay": timedelta(minutes=5),
     "max_active_runs": 1,
-    "template_searchpath": "/opt/python"  # Path to search for sql file
 }
 
 dag = DAG(
-    dag_id="wikipedia_pageview_data_sink",
+    dag_id="wikipedia_pageview_ingest_sink_data",
     start_date=airflow.utils.dates.days_ago(1),
     schedule_interval="@daily",
     default_args=default_args
 )
-
-def save_logs_to_minio():
-    # MinIO configuration
-    minio_endpoint = 'minio:9000'
-    minio_access_key = 'accesskey'
-    minio_secret_key = 'secretkey'
-    minio_bucket_name = 'airflow'
-
-    # Connect to MinIO
-    minio_client = Minio(
-        minio_endpoint, 
-        access_key=minio_access_key, 
-        secret_key=minio_secret_key, 
-        secure=False
-        )
     
-    # Read the log file
-    log_file_path = '/logs/wiki_ingest_task.log'
-    with open(log_file_path, 'r') as file:
-        log_content = file.read()
+log_volume = k8s.V1Volume(
+    name="airflow-log-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name="airflow-log-pvc"
+    )
+)
 
-    # Upload the log file to MinIO
-    minio_client.put_object(
-        minio_bucket_name, 
-        'logs/wiki_ingest_task.log',
-        log_content        
-        )
-
-volume = k8s.V1Volume(
+data_volume = k8s.V1Volume(
     name="airflow-data-volume",
     persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
         claim_name="airflow-data-pvc"
@@ -66,40 +43,41 @@ log_volume_mount = k8s.V1VolumeMount(
 
 data_volume_mount = k8s.V1VolumeMount(
     name="airflow-data-volume",
-    mount_path="/mnt/airflow/data",
+    mount_path="/tmp",
     read_only=False
 )
 
 ingest_data = KubernetesPodOperator(
     task_id="ingest_wiki_pageview",
-    image="python-image",
-    name="get_wiki_pageview",
+    image="ingest-wiki:0.0.1",
+    name="ingest_wiki_pageview",
     namespace="airflow",
-    volumes=[volume],
+    volumes=[data_volume, log_volume],
     volume_mounts=[data_volume_mount, log_volume_mount],
-    cmds=["python3"],
-    arguments=["/opt/airflow/scripts/download_wiki_pageview.py", "--context_variable", '{{ ts }}'],
+    env_vars= {
+        "EXECUTE_DATE": "{{ ts_nodash }}"
+    },
     in_cluster=True,
     is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
     execution_timeout=timedelta(minutes=10),
-    retries=2,
+    retries=1,
     image_pull_policy='IfNotPresent',
     service_account_name='airflow',
     get_logs=True,
     dag=dag,
 )
 
-fetch_data = KubernetesPodOperator(
-    task_id="fetch_wiki_pageview",
-    image="python",
-    name="fetch_wiki_pageview",
+sink_data = KubernetesPodOperator(
+    task_id="sink_wiki_pageview",
+    image="sink-wiki:0.0.1",
+    name="sink_wiki_pageview",
     namespace="airflow",
-    volumes=[volume],
+    volumes=[data_volume, log_volume],
     volume_mounts=[data_volume_mount, log_volume_mount],
-    cmds=["python3"],
-    arguments=["/opt/airflow/scripts/fetch_wiki_pageview.py", "--context_variable", '{{ ts }}'],
     in_cluster=True,
     is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
     execution_timeout=timedelta(minutes=30),
     retries=1,
     image_pull_policy='IfNotPresent',
@@ -108,10 +86,4 @@ fetch_data = KubernetesPodOperator(
     dag=dag,
 )
 
-save_logs_task = PythonOperator(
-    task_id='save_logs_to_minio',
-    python_callable=save_logs_to_minio,
-    dag=dag
-)
-
-ingest_data >> fetch_data >> save_logs_task
+ingest_data >> sink_data
