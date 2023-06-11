5e5d69552b6e46c44a2b31338604809b29cf777c
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-12 03:02:42 +0900

    revise the test python script about airflow variable

--- python/context_test/context_test.py
+++ python/context_test/context_test.py
@@ -1,9 +1,17 @@
 import os
+import sys
 
-def get_context(context: None):
-    print(context)
-    print("finished")
+
+def test():
+    try:
+        context = os.environ.get('EXECUTE_DATE', "None")
+        day, time = context.split("T")
+        print(f"Day: {day}")
+        print(f"Time: {time}")
+        return 0
+    except:
+        return 1
 
 if __name__ == "__main__":
-    context = os.environ.get('EXECUTE_DATE')
-    get_context(context=context)
+    result = test()
+    sys.exit(result)
