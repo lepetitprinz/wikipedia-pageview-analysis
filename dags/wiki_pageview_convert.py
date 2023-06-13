6e3ffca1f82f82a069d0936b0f911336d832cbe9
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-14 01:38:55 +0900

    rename python script (ingest_sink - > convert)

---
+++ dags/wiki_pageview_convert.py
@@ -0,0 +1,94 @@
+from datetime import timedelta
+
+import airflow.utils.dates
+from airflow import DAG
+from kubernetes.client import models as k8s
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+
+default_args = {
+    "owner": "yjkim",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "concurrency": 1,
+    "max_active_tasks": 1, 
+    "max_active_runs": 1,
+}
+
+dag = DAG(
+    dag_id="wikipedia_pageview_convert",
+    start_date=airflow.utils.dates.days_ago(1),
+    schedule_interval="@daily",
+    default_args=default_args
+)
+    
+log_volume = k8s.V1Volume(
+    name="airflow-log-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name="airflow-log-pvc"
+    )
+)
+
+data_volume = k8s.V1Volume(
+    name="airflow-data-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name="airflow-data-pvc"
+    )
+)
+
+log_volume_mount = k8s.V1VolumeMount(
+    name="airflow-log-volume",
+    mount_path="/log",
+    read_only=False
+)
+
+data_volume_mount = k8s.V1VolumeMount(
+    name="airflow-data-volume",
+    mount_path="/tmp",
+    read_only=False
+)
+
+ingest_data = KubernetesPodOperator(
+    task_id="ingest_wiki_pageview",
+    image="ingest-wiki:0.0.1",
+    name="ingest_wiki_pageview",
+    namespace="airflow",
+    volumes=[data_volume, log_volume],
+    volume_mounts=[data_volume_mount, log_volume_mount],
+    env_vars= {
+        "EXECUTE_DATE": "{{ ts_nodash }}"
+    },
+    in_cluster=True,
+    is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
+    execution_timeout=timedelta(minutes=10),
+    retries=1,
+    image_pull_policy='IfNotPresent',
+    service_account_name='airflow',
+    get_logs=True,
+    dag=dag,
+)
+
+convert_data = KubernetesPodOperator(
+    task_id="convert_wiki_pageview",
+    image="convert-wiki:0.0.1",
+    name="convert_wiki_pageview",
+    namespace="airflow",
+    volumes=[data_volume, log_volume],
+    volume_mounts=[data_volume_mount, log_volume_mount],
+    env_vars= {
+        "EXECUTE_DATE": "{{ ts_nodash }}"
+    },
+    in_cluster=True,
+    is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
+    execution_timeout=timedelta(minutes=30),
+    retries=1,
+    image_pull_policy='IfNotPresent',
+    service_account_name='airflow',
+    get_logs=True,
+    dag=dag,
+)
+
+ingest_data >> convert_data 
