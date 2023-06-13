72bdd3f2a01713fc0161793aee7e63a5d56c421e
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-13 23:13:02 +0900

    change the folder name (sink_wiki -> convert_wiki)

--- python/convert_wiki/convert_wiki_pageview.py
+++ python/convert_wiki/convert_wiki_pageview.py
@@ -1,5 +1,4 @@
 import os
-import psycopg2
 from typing import Dict
 from datetime import date, datetime, timedelta
 
@@ -7,7 +6,7 @@ from datetime import date, datetime, timedelta
 def _fetch_pageview() -> Dict[str, int]:
     # Read the file within the mounted volume
     result = {}
-    with open("/mnt/wikipageviews", "r") as f:
+    with open("/tmp/wikipageviews", "r") as f:
         for line in f:
             domain_code, page_title, view_counts, _ = line.split(" ")
             if domain_code == "en" and len(page_title) < 100:
@@ -31,34 +30,19 @@ def _calc_datetime():
 
     return execute_date, execute_time
 
-def sink_pageview(data: Dict[str, int]) -> None:
-    # Establish a connection to the PostgreSQL database
-    conn = psycopg2.connect(
-        host='localhost',
-        port='5432',
-        dbname='wiki',
-        user='postgres',
-        password='postgres'
-    )
-
-    # Create a cursor object to execute SQL statements
-    cursor = conn.cursor()
-
+def convert_to_sql(data: Dict[str, int]) -> None:
     # Get the time related data
     execute_date, execute_time = _calc_datetime()
 
-    # Execute SQL statements to save the data in the database
-    for page_name, pageview_cnt in data.items():
-        cursor.execute("INSERT INTO pageview_counts (execute_date, execute_time, page_name, pageview_cnt) VALUES (%s, %s, %s, %s)",
-                    (execute_date, execute_time, page_name, pageview_cnt))
-
-    # Commit the changes and close the cursor and connection
-    conn.commit()
-    print("Commited to database.")
-    cursor.close()
-    conn.close()
-    print("Database connection is closed.")
+    # Save pageview data to sql statement
+    with open("/tmp/wiki_pageview.sql", "w") as f:
+        for page_name, pageview_cnt in data.items():
+            f.write(
+                "INSERT INTO pageview_counts VALUES ("
+                f"'{execute_date}', '{execute_time}', '{page_name}', {pageview_cnt}"
+                ");\n"
+            )
 
 if __name__ == "__main__":
     pageview = _fetch_pageview()
-    sink_pageview(data=pageview)
+    convert_to_sql(data=pageview)
