33530ec8b22786f76a4612d08cbc6784f9e26c00
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-22 01:26:21 +0900

    create a dag that sink wiki pageview to postgresql

--- app/sink_wiki/sink_wiki_pageview.py
+++ app/sink_wiki/sink_wiki_pageview.py
@@ -1,4 +1,6 @@
+import os
 import time
+import shutil
 from pyspark.sql import SparkSession
 
 
@@ -13,10 +15,33 @@ def main(spark: SparkSession):
         .option("dbtable", "pageview_counts") \
         .option("user", "postgres") \
         .option("password", "postgres") \
+        .mode("append") \
         .save()
     
     print("Sinking data to postgresql is finished.")
+
     time.sleep(10)
+
+    # remove 
+    remove_data()
+
+def remove_data() -> None:
+    dir_path = "/mnt"
+
+    # Get a list of all files in the directory
+    file_list = os.listdir(dir_path)
+
+    # Iterate over the file list and remove each file
+    for file_name in file_list:
+        file_header = file_name.split("-")[0]
+        if file_header in ["liblz4", "spark", "hsperfdata_root"]:
+            file_path = os.path.join(dir_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+
+    print("Removing previous data is finished.")
 
 if __name__ == '__main__':
     # Create a SparSession
