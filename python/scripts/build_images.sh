7d8ffa4013fe1d3593493d76c0fe9bccff3d8901
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-06-22 01:28:58 +0900

    Delete build_images.sh

--- python/scripts/build_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Python scripts for ingesting wiki pageview 
-docker build --tag ingest-wiki:0.0.1 ingest_wiki/.
-kind load docker-image ingest-wiki:0.0.1 --name wiki-cluster
-
-# Python scripts for converting wiki pageview 
-docker build --tag convert-wiki:0.0.1 convert_wiki/.
-kind load docker-image convert-wiki:0.0.1 --name wiki-cluster
